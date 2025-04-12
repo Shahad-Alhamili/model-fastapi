fdfca681b8e1315c9eb6a0b7a9da7e6e66aa5fa3
shahad <132993961+Shahad-Alhamili@users.noreply.github.com>
2025-04-12 20:03:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ app = FastAPI()
 client = Client("shahad-alh/Arabi_char_classifier")
 
 @app.post("/classify")
-def classify_image(file: UploadFile = File(...)):
-    # Saving the uploaded image temporarily
+async def classify_image(file: UploadFile = File(...)):
+    # Save the uploaded file to a temporary location
     temp_path = f"temp_{file.filename}"
+    
+    with open(temp_path, "wb") as buffer:
+        content = await file.read()
+        buffer.write(content)
 
-    # Send image to HF Space
+    # Send image to Hugging Face Space
     try:
         prediction = client.predict(handle_file(temp_path), api_name="/predict")
     except Exception as e:
