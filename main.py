a113662dce62488eab5c2971c77f224afeab8853
shahad <132993961+Shahad-Alhamili@users.noreply.github.com>
2025-04-12 16:58:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ client = Client("shahad-alh/Arabi_char_classifier")
 
 @app.post("/classify")
 async def classify_image(file: UploadFile = File(...)):
-    # Saving the uploaded image temporarily
-    temp_path = f"temp_{file.filename}"
-
     # Send image to HF Space
     try:
-        prediction = client.predict(img = handle_file(temp_path), api_name="/predict")
+        prediction = client.predict(img = handle_file(file), api_name="/predict")
     except Exception as e:
         return {"error": str(e)}
 
