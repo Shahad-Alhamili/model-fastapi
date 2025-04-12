8a296cc0d2e148d4d03186df4a0614e1fcff18c0
shahad <132993961+Shahad-Alhamili@users.noreply.github.com>
2025-04-12 18:28:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from typing import Optional
 from fastapi import FastAPI, File, UploadFile
 from gradio_client import Client, handle_file
 
 app = FastAPI()
 client = Client("shahad-alh/Arabi_char_classifier")
 
-@app.post("/classify")
 def classify_image(file: UploadFile = File(...)):
     # Saving the uploaded image temporarily
     temp_path = f"temp_{file.filename}"
+
     # Send image to HF Space
     try:
         prediction = client.predict(handle_file(temp_path), api_name="/predict")
@@ -16,7 +15,7 @@ def classify_image(file: UploadFile = File(...)):
         return {"error": str(e)}
 
     return {"prediction": prediction}
-
+    
 @app.get("/")
 def root():
     return {"message": "Batoot is here!!"}
